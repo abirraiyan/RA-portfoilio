6499f9ef56382ee2495fd6a687aac2ba69a718ee
abirraiyan <raiyan.abir112@gmail.com>
2026-03-19 03:15:13 +0600

    Final fix for GSAP + TS

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,11 +1,10 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import ScrollSmoother from "gsap-trial/ScrollSmoother";
 import SplitText from "gsap-trial/SplitText";
-
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
-  split?: SplitText;
+  split?: any;
 }
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
